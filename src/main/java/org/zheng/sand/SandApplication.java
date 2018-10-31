5f2bf67e8f6a3ea0c97c15ecf86aa79b2338f97d
zhengguochun <zheng13239@163.com>
2018-10-31 13:30:13 +0800

    提交所有的项目基础结构到git

---
+++ src/main/java/org/zheng/sand/SandApplication.java
@@ -0,0 +1,12 @@
+package org.zheng.sand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandApplication.class, args);
+	}
+}
