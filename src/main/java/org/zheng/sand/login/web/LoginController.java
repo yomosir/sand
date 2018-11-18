ebd1b375faf6713edc389c6d5c2c468de29239b9
zhengguochun <zheng13239@163.com>
2018-11-18 22:37:35 +0800

    添加登录controller方法

--- src/main/java/org/zheng/sand/login/web/LoginController.java
+++ src/main/java/org/zheng/sand/login/web/LoginController.java
@@ -1,10 +1,26 @@
 package org.zheng.sand.login.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.zheng.sand.login.service.LoginService;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/admin")
 public class LoginController {
+    @Autowired
+    public LoginService loginService;
 
+    @RequestMapping(value = "/login",method = RequestMethod.PUT)
+    public String login(@RequestParam String username,@RequestParam String password){
+        boolean res = loginService.login(username,password);
+        if(res){
+            return "/admin/main";
+        }else{
+
+        }
+        return null;
+    }
 }
