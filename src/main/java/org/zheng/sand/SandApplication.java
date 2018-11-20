848250f6fff6ad99c08dc6172078dc548ad043e8
zhengguochun <zheng13239@163.com>
2018-11-20 22:42:55 +0800

    添加对mybatis的dao接口的扫描

--- src/main/java/org/zheng/sand/SandApplication.java
+++ src/main/java/org/zheng/sand/SandApplication.java
@@ -1,9 +1,11 @@
 package org.zheng.sand;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.zheng.sand.*.dao")
 public class SandApplication {
 
 	public static void main(String[] args) {
